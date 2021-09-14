12144e7bb2f83ce0c0675cd46d7cb66c0ff1bf39
NoaKafka <35343777+NoaKafka@users.noreply.github.com>
2021-09-14 17:04:02 +0900

    Update NW.py

--- NW.py
+++ NW.py
@@ -1,13 +1,3 @@
-1번문항 - 이유 :
-    관심 가진 계기 :
-        옥수역 귀신
-    일상이 되는 개발자, 내가 만드는 새로운 기능, 성능 개선이 세계 곳곳 의 사람들에게 편리함과 만족을 가져다 줬으면 좋겠다.
-2번문항 - MelGAN
-3번문항
-    1) 중간규모 프로그램 및 시스템을 개발할 수있으며, 주요 이슈 트러블슈팅을 할 수 있을 정도로 내부구조에 대해 이해하고 이씀.
-
-4번문항
-    1) 단점 : 시리즈의 경우 사용자가 웹툰을 볼 때 한번에 다운을 다 받을때 까지 기다림.
-            이는 소설을 보는 방식과 일치. 소설의 경우는 한번에 다운 받을 필요가 있을까
-            Why? 한 화면에 사용자가 머무는 시간은 소설이 더 길다. 그런데 굳이 한 화를 벌크로 내려받은 뒤에 보여줄 필요가 있는가
-    2) 장점 : 
+https://deque.tistory.com/108?category=984011
+https://github.com/5seunghoon/Kotlin-MVVM-Sample/tree/master/app/src/main/java/com/tistory/deque/kotlinmvvmsample
+https://deque.tistory.com/109?category=984011
